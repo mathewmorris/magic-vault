838bb377505381046b17839d24fb462264984617
Mathew Morris <10860014+mathewmorris@users.noreply.github.com>
2023-09-11 17:44:32 -0500

    Tesseract initialized

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import Link from "next/link";
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
@@ -12,7 +13,25 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      
+    <div>
+      <nav className="bg-blue-500 p-4">
+        <div className="container mx-auto">
+          <div className="flex justify-between">
+            <div className="text-white font-bold">Magic Vault</div>
+            <div>
+              <Link href="/">
+                  <span className="text-white mx-2">Home</span>
+              </Link>
+              <Link href="/about">
+                  <span className="text-white mx-2">About</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
       <Component {...pageProps} />
+    </div>
     </SessionProvider>
   );
 };
---
+++ src/pages/about.tsx
@@ -0,0 +1,18 @@
+import Reac from "react";
+import { Tesseract } from "tesseract.ts";
+
+const About: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl">About Magic Vault</h1>
+      <p className="mt-4">
+        Magic Vault is your go-to app for managing your Magic the Gathering card collection.
+        Whether you're building decks, tracking card values, or playtesting strategies,
+        Magic Vault has you covered!
+      </p>
+    </div>
+  );
+};
+
+export default About;
+
