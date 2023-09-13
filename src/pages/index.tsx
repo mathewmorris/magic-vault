30ddcd863b518c4ba8b5290375c177c94705d279
Mathew Morris <10860014+mathewmorris@users.noreply.github.com>
2023-09-12 21:54:32 -0500

    Camera feed added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, {useState}  from "react";
 import { Tesseract } from 'tesseract.ts';
+import CameraFeed from '~/components/CameraFeed';
+
+function useCaptureFrame (video: HTMLVideoElement) {
+    const canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    canvas
+        .getContext("2d")
+        .drawImage(video, 0, 0, canvas.width, canvas.height);
+    
+    return canvas.toDataURL("image/png");
+}
 
 const App = () => {
-  Tesseract.recognize(
-  'https://tesseract.projectnaptha.com/img/eng_bw.png',
-  'eng'
-).progress(console.log).then((res: any) => {
-  console.log(res);
-})
+const [text, setText] = useState("...reading text...");
 
-  return (
+(async () => {
+const worker = await Tesseract.create({
+  language: 'en',
+});
+  const data = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  setText(data.text);
+  await worker.terminate();
+})();
+return (
       <div className="container mx-auto p-4">
         <h1 className="text-4xl">Welcome to Magic Vault!</h1>
+        <div className="columns-3">
+            <div className="container p-2 border-4 rounded-lg border-indigo-500/100">
+                <CameraFeed />
+            </div>
+            <p>{text}</p>
+        </div>
       </div>
   );
 };
 
-export default App
-;
+export default App;
+
