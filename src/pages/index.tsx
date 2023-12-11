74a14cc2ebc197f548441a8df93b6a3acb1fa117
smoose <10860014+mathewmorris@users.noreply.github.com>
2023-12-11 04:50:20 -0700

    fix collection migration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import { api } from "~/utils/api";
 
 const App = () => {
-  const { data, error } = api.collection.getSecretMessage.useQuery();
-
   return (
-      <div className="container mx-auto p-4 dark:bg-slate-800">
+    <div className="container mx-auto p-4 dark:bg-slate-800">
       <h1 className="text-4xl">Welcome to Magic Vault!</h1>
-      {error ? (
-          <p className="text-red-200">{error.message}</p>
-          ) : (
-            <p>{data ?? 'hold on a sec...'}</p>
-            )
-      } 
-      </div>
-      );
+    </div>
+  );
 };
 
 export default App;
