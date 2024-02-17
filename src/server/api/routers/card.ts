11cf346038373dc8bc762f9996a3f1430a0f7416
smoose <10860014+mathewmorris@users.noreply.github.com>
2024-02-17 00:11:29 -0600

    search scryfall api on search When I search for a card, the server will hit the scryfall api search endpoint. This is awesome, cause now we'll get all cards in their database. Need to figure out a cache of sorts that persists between sessions since Scryfall has asked to do that. Not sure how to accomplish as of now. I added a TODO to create a Search model that will help me compare results in our db to the results saved in the Search table. Not sure how that would work but it might help? Essentially I want to check our cache (db) before I hit the Scryfall API. I'd save the different query parameters, total results, last time cached (which can be updated every 24 hours), and maybe the different unique ids returned (might be too granular).
    Anyway. Progress! Love it

--- src/server/api/routers/card.ts
+++ src/server/api/routers/card.ts
@@ -1,3 +1,5 @@
+import { type Card } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
@@ -5,11 +7,20 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
+interface ScryfallSearchResponse {
+  object: string;
+  total_cards: number;
+  has_more: boolean;
+  data: Card[];
+}
+
 export const cardRouter = createTRPCRouter({
   search: publicProcedure
   .input(z.string().min(1))
   .query(async ({ ctx, input }) => {
-    const matches = ctx.prisma.card.findMany({
+    // TODO: Create a searchRouter that will save results of search calls to scryfallAPI
+    // will use to see if search in db returns same number of results
+    const matches = await ctx.prisma.card.findMany({
       where: {
         name: {
           contains: input,
@@ -17,7 +28,32 @@ export const cardRouter = createTRPCRouter({
       }
     });
 
-    return matches;
+    const scryfallSearch = await fetch(`https://api.scryfall.com/cards/search?q=${input}`);
+
+    if (!scryfallSearch.ok) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Something went wrong when trying to search the Scryfall API.',
+        cause: scryfallSearch.statusText,
+      })
+    }
+
+    const cards = await scryfallSearch.json() as ScryfallSearchResponse;
+    await ctx.prisma.card.createMany({
+      data: cards.data.map((card) => ({
+        scryfall_id: card.id,
+        scryfall_uri: card.scryfall_uri,
+        name: card.name,
+        layout: card.layout,
+        image_status: card.image_status,
+        all_parts: card.all_parts,
+        card_faces: card.card_faces,
+        image_uris: card.image_uris,
+      })),
+    });
+    console.log('Has More?', cards.has_more);
+
+    return cards.data;
   }),
 });
 
