6ab35fe735c1fec4e7d43fc7f785aa229358c451
Mat Morris <hello@mathewmorris.com>
2024-05-08 23:06:29 -0500

    search scryfall api on /api/search (#12)
    * search scryfall api on search
    When I search for a card, the server will hit the scryfall api search endpoint.
    This is awesome, cause now we'll get all cards in their database.
    Need to figure out a cache of sorts that persists between sessions since Scryfall has asked to do that. Not sure how to accomplish as of now.
    I added a TODO to create a Search model that will help me compare results in our db to the results saved in the Search table. Not sure how that would work but it might help?
    Essentially I want to check our cache (db) before I hit the Scryfall API. I'd save the different query parameters, total results, last time cached (which can be updated every 24 hours), and maybe the different unique ids returned (might be too granular).
    
    Anyway. Progress! Love it
    
    * fix create types

--- src/server/api/routers/card.ts
+++ src/server/api/routers/card.ts
@@ -1,23 +1,59 @@
+import { type Prisma, type Card } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
-  createTRPCRouter,
-  publicProcedure,
+    createTRPCRouter,
+    publicProcedure,
 } from "~/server/api/trpc";
 
-export const cardRouter = createTRPCRouter({
-  search: publicProcedure
-  .input(z.string().min(1))
-  .query(async ({ ctx, input }) => {
-    const matches = ctx.prisma.card.findMany({
-      where: {
-        name: {
-          contains: input,
-        }
-      }
-    });
+interface ScryfallSearchResponse {
+    object: string;
+    total_cards: number;
+    has_more: boolean;
+    data: Card[];
+}
 
-    return matches;
-  }),
+export const cardRouter = createTRPCRouter({
+    search: publicProcedure
+    .input(z.string().min(1))
+    .query(async ({ ctx, input }) => {
+        // TODO: Create a searchRouter that will save results of search calls to scryfallAPI
+        // will use to see if search in db returns same number of results
+        // const matches = await ctx.prisma.card.findMany({
+        //     where: {
+        //         name: {
+        //             contains: input,
+        //         }
+        //     }
+        // });
+
+        const scryfallSearch = await fetch(`https://api.scryfall.com/cards/search?q=${input}`);
+
+        if (!scryfallSearch.ok) {
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: 'Something went wrong when trying to search the Scryfall API.',
+                cause: scryfallSearch.statusText,
+            })
+        }
+
+        const cards = await scryfallSearch.json() as ScryfallSearchResponse;
+        await ctx.prisma.card.createMany({
+            skipDuplicates: true,
+            data: cards.data.map((card) => ({
+                scryfall_id: card.id,
+                scryfall_uri: card.scryfall_uri,
+                name: card.name,
+                layout: card.layout,
+                image_status: card.image_status,
+                all_parts: card.all_parts as Prisma.JsonArray,
+                card_faces: card.card_faces as Prisma.JsonArray,
+                image_uris: card.image_uris as Prisma.JsonArray,
+            })),
+        });
+
+        return cards.data;
+    }),
 });
 
