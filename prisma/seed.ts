3a7aa9cdcc70de80b34b7d43ad2890b3b4098661
Mathew Morris <hello@mathewmorris.com>
2024-10-24 15:24:13 -0500

    seeding db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,37 +1,54 @@
-import { type Prisma, PrismaClient } from '@prisma/client';
-import fs from 'fs';
+import { PrismaClient, type Card } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
-type CardFromJsonFile = {
-  id: string,
-  name: string,
-  scryfall_uri: string,
-  image_status: string,
-  image_uris: Prisma.JsonObject,
-  card_faces: Prisma.JsonObject,
-  all_parts: Prisma.JsonObject,
-  layout: string,
+async function fetchDefaultCards(): Promise<Card[] | undefined> {
+  const headers = {
+    'User-Agent': "MagicVaultScryfallDataBroker/1.0",
+    'Accept': "application/json;q=1.0,*/*;q=0.9"
+  }
+
+  const downloadUriResponse = await fetch('https://api.scryfall.com/bulk-data/default-cards', { headers });
+
+  if (downloadUriResponse.ok) {
+    const { download_uri } = await downloadUriResponse.json() as Record<'download_uri', string>;
+
+    const bulk_data_response = await fetch(download_uri, { headers });
+
+    if (bulk_data_response.ok) {
+      const cards = await bulk_data_response.json() as Card[];
+      return Promise.resolve(cards);
+    }
+
+    return Promise.reject(new Error('The bulk data response is not available'));
+  }
+
+  return Promise.reject(new Error('The download link is not available'));
 }
 
-// TODO: Remove hard coded path (fetch file?)
+async function deleteCards() {
+  await prisma.card.deleteMany();
+  console.log('deleted cards');
+}
 
 async function main() {
-  const cards = JSON.parse(fs.readFileSync('./scryfall-data-broker/scryfall_downloads/default_cards_2024_10_11.json', 'utf8')) as CardFromJsonFile[];
-  await prisma.card.deleteMany()
-  console.log('deleted cards')
-  await prisma.card.createMany({
-    data: cards.map((card) => ({
-      id: card.id,
-      name: card.name,
-      scryfall_uri: card.scryfall_uri,
-      image_status: card.image_status,
-      image_uris: card.image_uris,
-      card_faces: card.card_faces,
-      all_parts: card.all_parts,
-      layout: card.layout,
-    }))
-  })
+  const cards = await fetchDefaultCards();
+
+  if (cards != null) {
+    await deleteCards();
+    await prisma.card.createMany({
+      data: cards.map((card) => ({
+        id: card.id,
+        name: card.name,
+        scryfall_uri: card.scryfall_uri,
+        image_status: card.image_status,
+        image_uris: card.image_uris ?? {},
+        card_faces: card.card_faces ?? {},
+        all_parts: card.all_parts ?? {},
+        layout: card.layout,
+      }))
+    })
+  }
 }
 
 main()
