61b421f62f2022bbf15c26375936429f511a28e1
Mathew Morris <hello@mathewmorris.com>
2024-10-24 03:09:11 -0500

    remove scryfall_id

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,37 @@
-import { PrismaClient } from '@prisma/client';
-import { cards } from './cards';
+import { type Prisma, PrismaClient } from '@prisma/client';
+import fs from 'fs';
 
 const prisma = new PrismaClient()
 
+type CardFromJsonFile = {
+  id: string,
+  name: string,
+  scryfall_uri: string,
+  image_status: string,
+  image_uris: Prisma.JsonObject,
+  card_faces: Prisma.JsonObject,
+  all_parts: Prisma.JsonObject,
+  layout: string,
+}
+
+// TODO: Remove hard coded path (fetch file?)
+
 async function main() {
+  const cards = JSON.parse(fs.readFileSync('./scryfall-data-broker/scryfall_downloads/default_cards_2024_10_11.json', 'utf8')) as CardFromJsonFile[];
   await prisma.card.deleteMany()
   console.log('deleted cards')
   await prisma.card.createMany({
     data: cards.map((card) => ({
+      id: card.id,
       name: card.name,
-      scryfall_id: card.id,
-      layout: card.layout,
+      scryfall_uri: card.scryfall_uri,
       image_status: card.image_status,
       image_uris: card.image_uris,
-      scryfall_uri: card.scryfall_uri,
+      card_faces: card.card_faces,
+      all_parts: card.all_parts,
+      layout: card.layout,
     }))
   })
-  console.log('cards seeded!')
-  await prisma.user.create({
-    data: {
-      id: '1234',
-      name: 'John',
-      email: 'john.magicvault@mathewmorris.com',
-    }
-  })
-  console.log('user seeded!')
 }
 
 main()
