7119f58fd2fe4425de388cde6a075bff513ddc8a
Mathew Morris <hello@mathewmorris.com>
2024-12-14 04:43:16 -0600

    scripts + index updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const CollectionList = ({ collections }: { collections: Collection[] }) => {
-  console.log(collections)
-  const create = api.collection.create.useMutation();
-
   if (collections.length < 1) {
     return (
       <>
         <p>No collections...</p>
-        <button className="rounded-full dark:bg-purple-950 px-4 py-2 font-semibold no-underline transition dark:hover:bg-pink-800 dark:focus:bg-pink-800 dark:hover:drop-shadow-glow dark:focus:drop-shadow-glow" onClick={() => create.mutate({ name: "My New Collection" })}>Create Collection</button>
       </>
     )
   }
@@ -26,16 +22,19 @@ const CollectionList = ({ collections }: { collections: Collection[] }) => {
 
 const App = () => {
   const { data: sessionData } = useSession();
-  const collections = api.collection.getAll.useQuery();
+  const { data: collections } = api.collection.getAll.useQuery();
+  const create = api.collection.create.useMutation();
+  console.log(create)
 
   return (
     <div className="container mx-auto grid justify-items-stretch">
       <h1 className="text-5xl flex-row justify-self-center py-4">Magic Vault</h1>
       {sessionData && (
-        <>
+        <div>
+          <button className="rounded-full dark:bg-purple-950 px-4 py-2 font-semibold no-underline transition dark:hover:bg-pink-800 dark:focus:bg-pink-800 dark:hover:drop-shadow-glow dark:focus:drop-shadow-glow" onClick={() => create.mutate({ name: "My New Collection" })}>Create Collection</button>
           <p>Your collections</p>
-          <CollectionList collections={collections.data ?? []} />
-        </>
+          <CollectionList collections={collections ?? []} />
+        </div>
       )}
     </div>
   )
