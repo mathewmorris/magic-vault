c47dbdc94ba03f4b32dafab098057628010cd5be
Mathew Morris <hello@mathewmorris.com>
2025-01-07 17:16:05 -0600

    organize tests

--- src/server/api/routers/collection.test.ts
+++ src/server/api/routers/collection.test.ts
@@ -3,7 +3,7 @@ import { createInnerTRPCContext } from "../trpc";
 import { appRouter } from "../root";
 import { TRPCError } from "@trpc/server";
 
-describe("collection api", () => {
+describe("collection procedures", () => {
     const userId = "user123";
     const collectionId = "collection456";
 
@@ -11,88 +11,100 @@ describe("collection api", () => {
         jest.clearAllMocks();
     });
 
-    test("getAll procedure should not return soft-deleted collections", async () => {
-        const dateNow = new Date();
-        const collection = {
-            id: collectionId,
-            name: "Collection",
-            cards: [],
-            userId,
-            updatedAt: dateNow,
-            createdAt: dateNow,
-            deletedAt: null,
-        }
-        const session = {
-            user: { id: userId, name: "John Doe" },
-            expires: "1",
-        }
+    describe("getAll", function() {
+        test("should not return soft-deleted collections", async () => {
+            const dateNow = new Date();
+            const collection = {
+                id: collectionId,
+                name: "Collection",
+                cards: [],
+                userId,
+                updatedAt: dateNow,
+                createdAt: dateNow,
+                deletedAt: null,
+            }
+            const session = {
+                user: { id: userId, name: "John Doe" },
+                expires: "1",
+            }
 
-        const ctx = createInnerTRPCContext({ session });
-        const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
+            const ctx = createInnerTRPCContext({ session });
+            const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
 
-        // The passing result is forced by my mock. Maybe an integration or e2e test would be better?
-        // Essentially, I need to create a few collections, soft delete some of those, then 
-        // check results of this query. This might test prisma too much though. Will need
-        // to think about it more.
-        prismaMock.collection.findMany.mockResolvedValue([collection]);
+            // The passing result is forced by my mock. Maybe an integration or e2e test would be better?
+            // Essentially, I need to create a few collections, soft delete some of those, then 
+            // check results of this query. This might test prisma too much though. Will need
+            // to think about it more.
+            prismaMock.collection.findMany.mockResolvedValue([collection]);
 
-        return expect(
-            caller.collection.getAll()
-        ).resolves.toStrictEqual([collection]);
+            return expect(
+                caller.collection.getAll()
+            ).resolves.toStrictEqual([collection]);
+        });
     });
 
-    test("softDelete should set `deletedAt` to date", async () => {
-        const dateNow = new Date();
-        const collection = {
-            id: collectionId,
-            name: "Collection",
-            cards: [],
-            userId,
-            updatedAt: dateNow,
-            createdAt: dateNow,
-            deletedAt: null,
-        }
-        const session = {
-            user: { id: userId, name: "John Doe" },
-            expires: "1",
-        }
+    describe("softDelete", function() {
+        test("should set `deletedAt` to date", async () => {
+            const dateNow = new Date();
+            const collection = {
+                id: collectionId,
+                name: "Collection",
+                cards: [],
+                userId,
+                updatedAt: dateNow,
+                createdAt: dateNow,
+                deletedAt: null,
+            }
+            const session = {
+                user: { id: userId, name: "John Doe" },
+                expires: "1",
+            }
 
-        const ctx = createInnerTRPCContext({ session });
-        const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
+            const ctx = createInnerTRPCContext({ session });
+            const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
 
-        prismaMock.collection.findUnique.mockResolvedValue(collection);
-        prismaMock.collection.update.mockResolvedValue({ ...collection, deletedAt: dateNow });
+            prismaMock.collection.findUnique.mockResolvedValue(collection);
+            prismaMock.collection.update.mockResolvedValue({ ...collection, deletedAt: dateNow });
 
-        return expect(
-            caller.collection.softDelete({ collectionId })
-        ).resolves.toStrictEqual({ ...collection, deletedAt: dateNow });
-    });
+            return expect(
+                caller.collection.softDelete({ collectionId })
+            ).resolves.toStrictEqual({ ...collection, deletedAt: dateNow });
+        });
 
-    test("softDelete should throw error when collection not owned", async () => {
-        const dateNow = new Date();
-        const collection = {
-            id: collectionId,
-            name: "Collection",
-            cards: [],
-            userId,
-            updatedAt: dateNow,
-            createdAt: dateNow,
-            deletedAt: null,
-        }
-        const session = {
-            user: { id: "notowner", name: "John Doe" },
-            expires: "1",
-        }
+        test("should throw error when collection not owned", async () => {
+            const dateNow = new Date();
+            const collection = {
+                id: collectionId,
+                name: "Collection",
+                cards: [],
+                userId,
+                updatedAt: dateNow,
+                createdAt: dateNow,
+                deletedAt: null,
+            }
+            const session = {
+                user: { id: "notowner", name: "John Doe" },
+                expires: "1",
+            }
 
-        const ctx = createInnerTRPCContext({ session });
-        const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
+            const ctx = createInnerTRPCContext({ session });
+            const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
 
-        prismaMock.collection.findUnique.mockResolvedValue(collection);
-        prismaMock.collection.update.mockResolvedValue({ ...collection, deletedAt: dateNow });
+            prismaMock.collection.findUnique.mockResolvedValue(collection);
+            prismaMock.collection.update.mockResolvedValue({ ...collection, deletedAt: dateNow });
 
-        return expect(
-            caller.collection.softDelete({ collectionId })
-        ).rejects.toStrictEqual(new TRPCError({ code: "FORBIDDEN", message: "You are not authorized to access this collection" }));
+            return expect(
+                caller.collection.softDelete({ collectionId })
+            ).rejects.toStrictEqual(new TRPCError({ code: "FORBIDDEN", message: "You are not authorized to access this collection" }));
+        });
+
+    })
+    describe('recoverCollection', function() {
+        test("should throw error when collection not owned", async function() {
+        });
+
+        test("should remove date from deletedAt field of owned collection", async function() {
+        });
     });
 });
 
