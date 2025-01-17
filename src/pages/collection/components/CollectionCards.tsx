4d6c3c8e4fd040d8a6df8573f90513ead8cacf87
Mathew Morris <hello@mathewmorris.com>
2025-01-17 05:11:17 -0600

    remove abstraction for now

--- src/pages/collection/components/CollectionCards.tsx
+++ src/pages/collection/components/CollectionCards.tsx
@@ -1,8 +1,6 @@
 import { Card, CardsOnCollections } from "@prisma/client";
-import { useState } from "react";
 import Button from "~/components/Button";
-import CardViewer from "~/components/CardViewer";
-import SearchBar from "~/components/SearchBar";
+import { api } from "~/utils/api";
 
 export type CollectionCard = CardsOnCollections & { card: Card };
 
@@ -11,21 +9,42 @@ type CollectionCardsProps = {
 }
 
 export function CollectionCards({ cards }: CollectionCardsProps) {
+  const mutation = api.collection.setCardCount.useMutation()
+  const context = api.useContext();
+
   if (!cards || cards.length <= 0) {
     return <div>No cards</div>
   }
 
-  const [showSearch, setShowSearch] = useState(false)
+  return cards.map(({ card, collectionId, count }) => {
+    async function addCard() {
+      await mutation.mutateAsync({ collectionId, cardId: card.id, count: count + 1 })
+      context.collection.byId.invalidate({ id: collectionId })
+    }
 
-  return (
-    <div className="p-4">
-      <Button onClick={() => setShowSearch(true)}>Add new card</Button>
-      {showSearch && (
-        <SearchBar onAddCard={() => { }} onRemoveCard={() => { }} />
-      )}
-      <CardViewer cards={cards} />
-    </div>
-  )
+    async function removeCard() {
+      if (count - 1 < 0) {
+        alert('you shouldn\'t be able to do that')
+      } else {
+        // TODO: Confirm user want to remove card? #configurable
+        await mutation.mutateAsync({ collectionId, cardId: card.id, count: count - 1 })
+      }
+      context.collection.byId.invalidate({ id: collectionId })
+    }
+
+    return (
+      <div key={card.id} className="flex justify-between">
+        <div>
+          <p>{card.name}</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <p>{count}</p>
+          <Button onClick={removeCard}>{count == 1 ? "Delete" : "-1"}</Button>
+          <Button onClick={addCard}>+1</Button>
+        </div>
+      </div>
+    )
+  })
 }
 
 export default CollectionCards
